b14cf5cab1b964b20a9607dfafdb607977b43e9b
ballock1 <ballock1@live.com>
2012-02-26 17:36:12 -0500

    Added /home and /killserv

---
+++ Commands/CmdHome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace MCDek
+{
+    public class CmdHome : Command
+    {
+
+        public override string name { get { return "home"; } }
+
+        public override string shortcut { get { return ""; } }
+
+        public override string type { get { return "other"; } }
+
+        public override bool museumUsable { get { return false; } }
+
+        public override LevelPermission defaultRank { get { return LevelPermission.Builder; } }
+
+        public CmdHome() { }
+        public override void Use(Player p, string message)
+        {
+            if (p != null)
+            {
+                    Command.all.Find("time").Use(p, msg);
+                    Command.all.Find("inbox").Use(p, msg);
+                    Command.all.Find("who").Use(p, message);
+                    
+            }
+        }
+
+
+        public override void Help(Player p)
+        {
+            Player.SendMessage(p, "/home - Tells you time, checks your inbox, who is online, and more.");
+        }
+    }
+}
---
+++ Commands/CmdKillServ.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace MCDek
+{
+    public class CmdKillServ : Command
+    {
+        public override string name { get { return "killserv"; } }
+        public override string shortcut { get { return ""; } }
+        public override string type { get { return "other"; } }
+        public override bool museumUsable { get { return true; } }
+        public override LevelPermission defaultRank { get { return LevelPermission.Operator; } }
+        public CmdKillServ() { }
+
+       public override void Use(Player p, string message)
+        {
+            try
+            {
+                if (message == "") { Help(p); return; }
+                if (p != null && foundGroup.Permission >= p.group.Permission)
+                   {
+                   who = Player.players;
+                   if (p != null)
+                      {
+                         killMsg = " was killed by " + p.color + p.name;
+                      }
+                   else
+                      {
+                         killMsg = " was killed by " + "the Console.";
+                      }
+                    }
+             
+            who.HandleDeath(Block.rock, killMsg);
+           }
+        public override void Help(Player p)
+        {
+            Player.SendMessage(p, "Kills everyone in the server other than the owner");
+        }
+    }
+}
