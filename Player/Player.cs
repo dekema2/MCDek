9dd97f374f1717be246f8475157307742d357136
dekema2 <dekema2@me.com>
2012-03-09 18:12:19 -0500

    Update Player/Player.cs

--- Player/Player.cs
+++ Player/Player.cs
@@ -12,6 +12,9 @@
 	or implied. See the License for the specific language governing
 	permissions and limitations under the License.
 */
+
+//From dekema2: This will be heavily modified in some way or another, this one file determines our success.
+
 using System;
 using System.Threading;
 using System.Net;
